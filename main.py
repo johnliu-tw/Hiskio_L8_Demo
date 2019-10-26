021c916f57c1b694ad6dd04ba68ae03a4c6c0c8b
johnliu <johnliutw@hotmail.com>
2019-10-26 19:37:14 +0800

    remove crawler

--- main.py
+++ main.py
@@ -9,14 +9,6 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route('/get_data')
-def get_data():
-  connection = pymysql.connect (host='localhost',
-                              user='root',
-                              password='password',
-                              db='new_media',
-                              cursorclass=pymysql.cursors.DictCursor)
-    return "Hello World!"
 
 if __name__ == '__main__':
     app.run()
